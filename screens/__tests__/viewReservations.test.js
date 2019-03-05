14e24da0f3a37121905dd3609bff152f4be829ef
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2019-03-05 00:18:01 -0600

    Remove unused tests

--- screens/__tests__/viewReservations.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-describe('Mock receiving reservations back from Prisma DB', function () {
-    it('Mocks graphQL query', function () {
-        var fetchReservations = jest.fn();
-        fetchReservations();
-        expect(fetchReservations).toHaveBeenCalled();
-    });
-});
--- screens/__tests__/viewReservations.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Mock receiving reservations back from Prisma DB', () => {
-  it('Mocks graphQL query', () => {
-    const fetchReservations = jest.fn();
-    fetchReservations();
-    expect(fetchReservations).toHaveBeenCalled();
-  });
-});
